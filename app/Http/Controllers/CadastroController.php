ead9d2b5e83ac6fceecc7f71f98fc3c682a46207
Daniel Caetano <danielscaetano2008@gmail.com>
2026-03-19 17:25:19 -0300

    só formatei

--- app/Http/Controllers/CadastroController.php
+++ app/Http/Controllers/CadastroController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
 class CadastroController extends Controller
 {
     /**
@@ -10,7 +11,7 @@ class CadastroController extends Controller
      */
     public function index()
     {
-                return view('cadastro',);
+        return view('cadastro');
 
     }
 
