d4486f2fe4d52242355df67e618360f2410a0c6e
Daniel Caetano <danielscaetano2008@gmail.com>
2026-03-19 17:25:19 -0300

    só formatei

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+
 class LoginController extends Controller
 {
     /**
@@ -11,9 +12,9 @@ class LoginController extends Controller
      */
     public function index()
     {
-                return view('login');
+        return view('login');
     }
-    
+
     /**
      * Show the form for creating a new resource.
      */
@@ -70,7 +71,7 @@ class LoginController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Request $request,string $id)
+    public function destroy(Request $request, string $id)
     {
     }
 }
