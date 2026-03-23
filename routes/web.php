ca40c928141be7a5be3c5b3562d23ca9bca82ec0
Daniel Caetano <danielscaetano2008@gmail.com>
2026-03-23 15:09:41 -0300

    Dani fez caquinha, ainda quero a branch e vou fazer a boa ali pra ele porque sou um bom amigo Revert "Merge pull request #8 from danielscaetano/muitos-pedidos-na-tela"
    This reverts commit 442c1201896b3ce14c58ea4d59d0e030d93af6fe, reversing
    changes made to a1d144fddce9aff1c8df6e256fd6b8a0ec57951e.

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'home']);
 
 Route::resource('produtos', ProdutoController::class);
-Route::resource('pedido', PedidoController::class);
 
 Route::resource('categorias', CategoriaController::class);
 
